4f71edc692e12abc1420d242fe528401f53c3fb5
Prajivinn T C <108303914+prajivinn@users.noreply.github.com>
2024-01-03 16:09:02 +0530

    Update Dominos Sales Analysis_SQL.sql

--- Dominos Sales Analysis_SQL.sql
+++ Dominos Sales Analysis_SQL.sql
@@ -31,22 +31,39 @@ GROUP BY month
 ORDER BY revenue DESC;
 
 -- 4. Are there any pizzas we should take off the menu, or any promotions we could leverage?
-SELECT pizza_id, SUM(quantity) AS total_sold
-FROM Order_Details
-GROUP BY pizza_id
-ORDER BY total_sold
-; 
+SELECT od.pizza_id, SUM(od.quantity) AS totalunitssold, (p.Price * SUM(od.quantity)) AS totalrevenue
+FROM order_details od 
+INNER JOIN pizzas p 
+        ON od.pizza_id = p.pizza_id
+GROUP BY od.pizza_id
+ORDER BY totalunitssold
+;
+
 
 -- 5. What is the average order value for each pizza category (e.g., Vegetarian, Non-Vegetarian, etc.)?
-SELECT PT.Category, AVG(P.Price * OD.quantity) AS avg_order_value
-FROM Pizzas P
-JOIN Order_Details OD ON P.pizza_id = OD.pizza_id
-JOIN Pizza_Types PT ON P.pizza_type_id = PT.pizza_type_id
-GROUP BY PT.Category
-ORDER BY avg_order_value DESC;
+WITH table1 AS (
+SELECT o.order_id, o.date, od.pizza_id, od.quantity, CASE
+  WHEN od.quantity = 4 THEN 4*p.price
+  WHEN od.quantity = 3 THEN 3*p.price
+  WHEN od.quantity = 2 THEN 2*p.price
+ELSE p.price
+END AS tprice, p.pizza_type_id, pt.Category
+FROM orders o 
+INNER JOIN order_details od
+        ON o.order_id = od.order_id
+INNER JOIN pizzas p
+        ON od.pizza_id = p.pizza_id
+INNER JOIN pizza_types pt
+        ON p.pizza_type_id = pt.pizza_type_id
+)
+
+SELECT Category, AVG(tprice) AS Average_Order_Value
+FROM table1
+GROUP BY Category
+ORDER BY Average_Order_Value desc;
 
 -- 6. Are there any trends in sales based on the day of the week?
- 
+
 SELECT
     DAYNAME(date) AS day_of_week,
     SUM(Price * quantity) AS total_sales
@@ -55,6 +72,3 @@ JOIN Order_Details ON Orders.order_id = Order_Details.order_id
 JOIN Pizzas ON Order_Details.pizza_id = Pizzas.pizza_id
 GROUP BY day_of_week
 ORDER BY total_sales DESC;
-
-
-
